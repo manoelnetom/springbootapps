102aeaae596e2c6d8ede3a010724562d6965808d
Manoel Neto <manoelnetom@gmail.com>
2022-05-05 14:26:53 -0300

    Update SecurityConfigurations.java

--- apifilmes/src/main/java/br/com/apifilmes/config/security/SecurityConfigurations.java
+++ apifilmes/src/main/java/br/com/apifilmes/config/security/SecurityConfigurations.java
@@ -28,8 +28,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		//.antMatchers(HttpMethod.GET,"/filmes").permitAll()
-		//.antMatchers(HttpMethod.GET,"/filmes/*").permitAll()
+		.antMatchers(HttpMethod.GET,"/filmes").permitAll()
+		.antMatchers(HttpMethod.GET,"/filmes/*").permitAll()
 		.anyRequest().authenticated()
 		.and().formLogin();
 		
